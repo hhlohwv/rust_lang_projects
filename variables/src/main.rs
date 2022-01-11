77da2625c2dd9dd8e94e3c85153f12d2c25f9be9
Harrison Loh <haloh@mix.wvu.edu>
2022-01-11 16:38:57 -0500

    Finishing up 3.1 - Variables and Mutability section of the rust book.

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,7 +1,7 @@
 // Bookmark: https://doc.rust-lang.org/book/ch03-01-variables-and-mutability.html
 
 // fn main() {
-//     let x = 5;  // not sure why the : i32, but I guess these things need to be explicitly declared? Just an autofill from VS Code, so will ignore for now
+//     let x = 5;
 //     println!("The value of x is: {}", x);
 
 //     x = 6; // error thrown because the immutable x is attempted to be set as another value.
@@ -25,27 +25,42 @@
 //     println!("{}", THREE_HOURS_IN_SECONDS)
 // }
 
-fn main() {
-    // first scope
-    let x = 5; // immutable variable assignment
-    println!("Initial x value is: {}", x);
+// fn main() {
+//     // first scope
+//     let x = 5; // immutable variable assignment
+//     println!("Initial x value is: {}", x);
 
-    let x = x + 1; // 'shadowing' the variable x, incrementing it by 1. Also works without 'let' if initial x is modified by 'mut'
-    // allows a variable to be changed but still immutable
-    println!("New value of x plus 1: {}", x);
+//     let x = x + 1; // 'shadowing' the variable x, incrementing it by 1. Also works without 'let' if initial x is modified by 'mut'
+//     // allows a variable to be changed but still immutable
+//     println!("New value of x plus 1: {}", x);
 
-    // second scope
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {}", x);
+//     // second scope
+//     {
+//         let x = x * 2;
+//         println!("The value of x in the inner scope is: {}", x);
 
-        let y = 10;
-        println!("Initial setting 2nd scope variable 'y' as: {}", y);
-    }
+//         let y = 10;
+//         println!("Initial setting 2nd scope variable 'y' as: {}", y);
+//     }
 
-    println!("The value of x post 2nd scope is: {}", x);
-    // println!("To reiterate, the value of 2nd scope variable 'y' is: {}", y);
+//     println!("The value of x post 2nd scope is: {}", x);
+//     // println!("To reiterate, the value of 2nd scope variable 'y' is: {}", y);
 
-    // first time working with scopes, but essentially it looks like whatever is available in the outer scope is usable by the inner one
-    // if a variable is defined only in an inner scope, then outer scopes don't have access to it
-}
+//     // first time working with scopes, but essentially it looks like whatever is available in the outer scope is usable by the inner one
+//     // if a variable is defined only in an inner scope, then outer scopes don't have access to it
+// }
+
+// fn main() {
+//     // changing the data type of a variable using shadowing (i.e. two let statements)
+//     let spaces = "     ";  // defining a variable with just spaces as a string
+//     println!("spaces before change = {}", spaces);
+
+//     let spaces = spaces.len();  // this length is not the number of characters, but rather the byte length of the variable
+//     println!("spaces = {}", spaces);
+
+//     // trying the same thing as above except with spaces being a mutable variable
+//     let mut spaces = "     ";
+//     spaces = spaces.len(); // error raised, can't change the data type from a string to a number
+// }
+
+// Finished with 3.1 Variables and Mutability section of the rust book!
